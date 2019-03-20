89e7e6efd21f313c215a325c43e4d4832ad21223
Isaac Murchie <isaac.appium@gmail.com>
2019-03-20 10:54:11 -0400

    Add ability to use yamllint if available

--- README.md
+++ README.md
@@ -2,12 +2,9 @@
 
 A simple [YAML](https://yaml.org/) linter plugin for [gulp](https://gulpjs.com/).
 
-Currently it just parses the YAML, and reports errors. This is, of course, not
+If [yamllint](https://github.com/adrienverge/yamllint) is installed, it will
+be executed against every YAML file found.
+
+If not, it will simply parse the file and report errors. This is, of course, not
 really linting, and the error messages are generally reported for the line
-_after_ the one that is actually a problem.
-
-It does, however, provide some feedback.
-
-### Future
-
-Either port a linter into JS, or use an external one.
+_after_ the one that is actually a problem. It does, however, provide some feedback.
--- index.js
+++ index.js
@@ -4,27 +4,49 @@ const yaml = require('js-yaml');
 const { EOL } = require('os');
 const PluginError = require('plugin-error');
 const red = require('ansi-red');
+const { exec } = require('child_process');
+const which = require('which');
 
 
-function lint (file, cb) {
-	jsYaml(file);
-	cb();
+function lint (file, opts, cb) {
+	// if the `yamllint` program is available, use it
+	// otherwise just try parsing the YAML
+	which('yamllint', function (err, resolvedPath) {
+		if (err) {
+			jsYaml(file, opts, cb);
+		} else {
+			yamllint(file, opts, cb);
+		}
+	});
 }
 
-function jsYaml (file) {
+function jsYaml (file, opts, cb) {
 	try {
-		(options.safe ? yaml.safeLoad : yaml.load)(file.contents.toString(enc));
+		(opts.safe ? yaml.safeLoad : yaml.load)(file.contents.toString(file.enc));
 	} catch (err) {
 		file.error = err;
 	}
+	cb();
+}
+
+function yamllint (file, opts, cb) {
+	exec(`yamllint ${file.path}`, function (err, stdout, stderr) {
+		if (err) {
+			err.stdout = stdout;
+			err.stderr = stderr;
+			file.error = err;
+		}
+		cb();
+	});
 }
 
 function gulpYamlLint (options = {}) {
   return new Transform({
     objectMode: true,
     transform (file, enc, cb) {
+			file.enc = enc;
       file.error = null;
-      lint(file, function () {
+      lint(file, options, function () {
 				cb(null, file);
 			});
     },
@@ -36,15 +58,28 @@ function handleError (files) {
     return;
   }
 
+	let fullLint = false;
 	for (const file of files) {
+		if (file.error.stdout) {
+			fullLint = true;
+		}
 		log.error(`Invalid YAML file: '${file.path}'`);
 		for (const line of file.error.message.split(EOL)) {
 			log.error(`> ${line}`);
 		}
+		for (const line of (file.error.stdout || '').split(EOL)) {
+			log.error(`> ${line}`);
+		}
+		for (const line of (file.error.stderr || '').split(EOL)) {
+			log.error(`> ${line}`);
+		}
 	}
 
-  log.error(`YAML ${files.length === 1 ? 'error' : 'errors'} found. Due to the limitations of YAML linting, the error `);
-  log.error(`is most likely in the line immediately ${red('before')} the line reported.`);
+  log.error(`YAML ${files.length === 1 ? 'error' : 'errors'} found.`);
+	if (!fullLint) {
+		log.error(`Due to the limitations of YAML linting, the error `);
+  	log.error(`is most likely in the line immediately ${red('before')} the line reported.`);
+	}
 }
 
 gulpYamlLint.failOnError = function () {
